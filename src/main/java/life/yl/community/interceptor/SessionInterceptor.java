28cba27738cbd93e2249b9302e9286db1c438fc2
yanglin <695836952@qq.com>
2019-12-06 18:17:08 +0800

    genetar

--- src/main/java/life/yl/community/interceptor/SessionInterceptor.java
+++ src/main/java/life/yl/community/interceptor/SessionInterceptor.java
@@ -2,6 +2,7 @@ package life.yl.community.interceptor;
 
 import life.yl.community.mapper.UserMapper;
 import life.yl.community.model.User;
+import life.yl.community.model.UserExample;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
@@ -11,6 +12,7 @@ import org.springframework.web.servlet.ModelAndView;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.List;
 
 /**
  * @author yanglin
@@ -28,9 +30,12 @@ public class SessionInterceptor implements HandlerInterceptor {
       for (Cookie cookie : cookies) {
         if (cookie.getName().equals("token")) {
           String token = cookie.getValue();
-          User user = userMapper.findByToken(token);
-          if (user != null) {
-            request.getSession().setAttribute("user", user);
+          UserExample example = new UserExample();
+          example.createCriteria()
+                  .andTokenEqualTo(token);
+          List<User> users = userMapper.selectByExample(example);
+          if (users.size() != 0) {
+            request.getSession().setAttribute("user", users.get(0));
           }
           break;
         }
--- src/main/java/life/yl/community/service/UserService.java
+++ src/main/java/life/yl/community/service/UserService.java
@@ -2,8 +2,11 @@ package life.yl.community.service;
 
 import life.yl.community.mapper.UserMapper;
 import life.yl.community.model.User;
+import life.yl.community.model.UserExample;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 /**
  * @author yanglin
@@ -17,19 +20,29 @@ public class UserService {
 
 
   public void createOrUpdate(User user) {
-    User dbUser = userMapper.findByAccountId(user.getAccountId());
-    if(dbUser == null){
+    UserExample example = new UserExample();
+    example.createCriteria()
+            .andAccountIdEqualTo(user.getAccountId());
+    List<User> users = userMapper.selectByExample(example);
+    if(users.size() == 0 ){
       //插入
       user.setGmtCreate(System.currentTimeMillis());
       user.setGmtModified(user.getGmtCreate());
       userMapper.insert(user);
     }else {
       //更新
-      dbUser.setGmtModified(System.currentTimeMillis());
-      dbUser.setAvatarUrl(user.getAvatarUrl());
-      dbUser.setName(user.getName());
-      dbUser.setToken(user.getToken());
-      userMapper.update(dbUser);
+      User dbUser = users.get(0);
+
+      User updateUser = new User();
+      updateUser.setGmtModified(System.currentTimeMillis());
+      updateUser.setAvatarUrl(user.getAvatarUrl());
+      updateUser.setName(user.getName());
+      updateUser.setToken(user.getToken());
+
+      UserExample userExample = new UserExample();
+      userExample.createCriteria()
+              .andIdEqualTo(dbUser.getId());
+      userMapper.updateByExampleSelective(updateUser,userExample);
     }
   }
 
