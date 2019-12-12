3be4d49c4d2a6c04d0d2a912cc75bb1b6031a947
yanglin <695836952@qq.com>
2019-12-12 23:59:44 +0800

    sb

--- src/main/resources/static/js/community.js
+++ src/main/resources/static/js/community.js
@@ -65,32 +65,57 @@ function collapseComments(e) {
         e.removeAttribute("data-collapse");
         e.classList.remove("active");
     }else {
-        $.getJSON("/comment/"+id,function (data) {
-           console.log(data);
-            var commentBody = $("comment-body-"+id);
-            var items = [];
-            commentBody.appendChild();
-            $.each(data.data,function (comment) {
+        var subCommentContainer = $("#comment-"+id);
 
-                items.push("<li id='"+key+)
-            });
-
-            $("<div/>",{
-                "class":"col-lg-12 col-md-12 col-sm-12 col-xs-12 collapse sub-comments",
-                "id":"comment-"+id,
-                html:items.join("")
-            }).appendTo(commentBody);
-
+        if(subCommentContainer.children().length != 1){
             //展开耳机评论
             comments.addClass("in");
             //标记二级评论展开状态
             e.setAttribute("data-collapse","in");
             e.classList.add("active");
-        });
+        }else {
+            $.getJSON("/comment/"+id,function (data) {
+
+                $.each(data.data.reverse(),function (index,comment) {
 
 
 
+                    var mediaLeftElement = $("<div/>",{
+                        "class":"media-left"
+                    }).append($("<img/>",{
+                        "class":"media-object img-rounded",
+                        "src":comment.user.avatarUrl
+                    }));
 
+                    var mediaBodyElement =  $("<div/>",{
+                        "class":"media-body"
+                    }).append($("<h5/>",{
+                        "class":"media-heading",
+                        "html":comment.user.name
+                    })).append($("<div/>",{
+                        "html":comment.content
+                    })).append($("<div/>",{
+                        "class":"menu"
+                    }).append($("<span/>",{
+                        "class":"pull-right",
+                        "html":moment(comment.gmtCreate).format('YYYY-MM-DD HH:mm')
+                    })));
+
+                    var mediaElement = $("<div/>",{
+                        "class":"media"
+                    }).append(mediaLeftElement).append(mediaBodyElement);
+
+                    var commentElement = $("<div/>",{
+                        "class":"col-lg-12 col-md-12 col-sm-12 col-xs-12 comments"
+                    }).append(mediaElement);
+                    subCommentContainer.prepend(commentElement);
+                });
+                //展开耳机评论
+                comments.addClass("in");
+                //标记二级评论展开状态
+                e.setAttribute("data-collapse","in");
+                e.classList.add("active");
+            });
+        }
     }
-
 }
