18c061d685e1d10f975fc335a69e6c43f57d3150
yanglin <695836952@qq.com>
2019-12-13 17:55:10 +0800

    标签栏的扩展与实现

--- src/main/resources/static/js/community.js
+++ src/main/resources/static/js/community.js
@@ -119,3 +119,20 @@ function collapseComments(e) {
         }
     }
 }
+
+function selectTag(value) {
+    var previous = $("#tag").val();
+
+    if(previous.indexOf(value) == -1){
+        if(previous){
+            $("#tag").val(previous+','+value);
+        }else {
+            $("#tag").val(value);
+        }
+    }
+
+}
+
+function showSelectTag() {
+    $("#select-tag").show();
+}
